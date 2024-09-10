0e99b65e24c2a6075a192549e6a8f742e5d7388e
Ishizuka13 <miguelishizuka4@gmail.com>
2024-09-09 23:24:56 -0300

    created test to return an AppError if the required params are not provided

--- src/enterprise/entities/lend.entity.js
+++ src/enterprise/entities/lend.entity.js
@@ -1,3 +1,5 @@
+const { AppError } = require("../../shared/errors");
+
 const lendsEntity = function () {
   function calculateDaysLate({ data_retorno, data_devolucao }) {
     return (
@@ -5,6 +7,9 @@ const lendsEntity = function () {
     );
   }
   const calculateFine = function ({ data_retorno, data_devolucao }) {
+    if (!data_devolucao || !data_retorno)
+      throw new AppError(AppError.requiredParams);
+
     const daysLate = calculateDaysLate({ data_retorno, data_devolucao });
 
     return `Multa por atraso: R$ ${daysLate > 0 ? "10,00" : "0"}`;
--- src/enterprise/entities/lend.entity.spec.js
+++ src/enterprise/entities/lend.entity.spec.js
@@ -1,3 +1,4 @@
+const { AppError } = require("../../shared/errors");
 const lendEntity = require("./lend.entity");
 
 describe("Lends entity", function () {
@@ -9,12 +10,17 @@ describe("Lends entity", function () {
 
     expect(output).toBe("Multa por atraso: R$ 0");
   });
-  test("calculateFine - no delay", function () {
+  test("calculateFine - com delay", function () {
     const output = lendEntity.calculateFine({
       data_devolucao: "2024-02-17",
       data_retorno: "2024-02-16",
     });
 
     expect(output).toBe("Multa por atraso: R$ 10,00");
+  });
+  test("calculateFine - return a throw AppError if the required inputs are not provided", function () {
+    expect(() => lendEntity.calculateFine({})).toThrow(
+      new AppError(AppError.requiredParams)
+    );
   });
 });
