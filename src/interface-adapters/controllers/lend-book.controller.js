29af2d150670bb0d8eec8fcd7d8e2e2da7dddb17
Ishizuka13 <miguelishizuka4@gmail.com>
2024-10-05 13:14:09 -0300

    created method to lend book and its tests

---
+++ src/interface-adapters/controllers/lend-book.controller.js
@@ -0,0 +1,43 @@
+const { z } = require("zod");
+const httpResponse = require("../../shared/helpers/http.response");
+const { AppError } = require("../../shared/errors");
+
+const zodValidator = z.object({
+  livro_id: z.number({
+    required_error: "Livro é obrigatório",
+  }),
+  usuario_id: z.number({
+    required_error: "Usuário é obrigatório",
+  }),
+  data_saida: z.string({
+    required_error: "Data saída é obrigatória",
+  }),
+  data_retorno: z.string({
+    required_error: "Data retorno é obrigatória",
+  }),
+});
+
+module.exports = async function lendBookController({
+  lendBookUseCase,
+  httpRequest,
+}) {
+  const bodyCheck = !lendBookUseCase || !httpRequest || !httpRequest.body;
+
+  if (bodyCheck) throw new AppError(AppError.dependencies);
+
+  const { livro_id, usuario_id, data_saida, data_retorno } = zodValidator.parse(
+    httpRequest.body
+  );
+
+  const output = await lendBookUseCase({
+    livro_id,
+    usuario_id,
+    data_saida: new Date(data_saida),
+    data_retorno: new Date(data_retorno),
+  });
+
+  return output.fold(
+    (err) => httpResponse(400, err.message),
+    () => httpResponse(201, null)
+  );
+};
---
+++ src/interface-adapters/controllers/lend-book.controller.spec.js
@@ -0,0 +1,80 @@
+const { ZodError } = require("zod");
+const { Either, AppError } = require("../../shared/errors");
+const httpResponse = require("../../shared/helpers/http.response");
+const lendBookController = require("./lend-book.controller");
+
+describe("lend Book Controller", function () {
+  const lendBookUseCase = jest.fn();
+
+  test("Must return a httpResponse 200 and null if success on register", async function () {
+    lendBookUseCase.mockResolvedValue(Either.Right(null));
+
+    const httpRequest = {
+      body: {
+        livro_id: 1,
+        usuario_id: 1,
+        data_saida: "2024-02-03",
+        data_retorno: "2024-02-15",
+      },
+    };
+
+    const response = await lendBookController({
+      lendBookUseCase,
+      httpRequest,
+    });
+
+    expect(response).toEqual(httpResponse(201, null));
+    expect(lendBookUseCase).toHaveBeenCalledWith({
+      livro_id: 1,
+      usuario_id: 1,
+      data_saida: expect.any(Date),
+      data_retorno: expect.any(Date),
+    });
+    expect(lendBookUseCase).toHaveBeenCalledTimes(1);
+  });
+
+  test("Must return a httpResponse 400 and error message if not success on register", async function () {
+    lendBookUseCase.mockResolvedValue(
+      Either.Left({ message: "validacao_invalida" })
+    );
+
+    const httpRequest = {
+      body: {
+        livro_id: 1,
+        usuario_id: 1,
+        data_saida: "2024-02-03",
+        data_retorno: "2024-02-15",
+      },
+    };
+
+    const response = await lendBookController({
+      lendBookUseCase,
+      httpRequest,
+    });
+
+    expect(response).toEqual(httpResponse(400, "validacao_invalida"));
+    expect(lendBookUseCase).toHaveBeenCalledWith({
+      livro_id: 1,
+      usuario_id: 1,
+      data_saida: expect.any(Date),
+      data_retorno: expect.any(Date),
+    });
+    expect(lendBookUseCase).toHaveBeenCalledTimes(1);
+  });
+
+  test("Must return a throw AppError if required params are not provided", async function () {
+    expect(() => lendBookController({})).rejects.toThrow(
+      new AppError(AppError.dependencies)
+    );
+  });
+
+  test("Must return a throw ZodValidator error if error on validation of required data", async function () {
+    const httpRequest = {
+      body: {},
+    };
+
+    expect(() =>
+      lendBookController({ lendBookUseCase, httpRequest })
+    ).rejects.toBeInstanceOf(ZodError);
+  });
+});
